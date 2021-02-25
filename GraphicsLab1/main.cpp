924741daa3f7503c5da4727b448e2020e5469b7f
zadolbali10 <anon.yu@mail.ru>
2021-02-26 01:44:36 +0700

    Implemented base

---
+++ GraphicsLab1/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include "glut.h"
+
+const int WIDTH = 400, HEIGHT = 400;
+GLubyte ColorR = 0, ColorG = 0, ColorB = 0;
+GLubyte PointSize = 5;
+
+// Функция вывода на экран 
+void Display(void)
+{
+   glClearColor(0.5, 0.5, 0.5, 1); glClear(GL_COLOR_BUFFER_BIT);
+   glColor3ub(ColorR, ColorG, ColorB);
+   glPointSize(PointSize);
+
+   glFinish();
+}
+
+int main(int argc, char** argv)
+{
+   glutInit(&argc, argv);
+   glutInitDisplayMode(GLUT_RGB);
+   glutInitWindowSize(WIDTH, HEIGHT);
+   glutCreateWindow("Пожилое приложение");
+
+   glutDisplayFunc(Display);
+
+   glutMainLoop();
+}
